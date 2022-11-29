764b783805c5884a9df4abb39cf78782e8d72624
Mohana <mohanakumari0330@gmail.com>
2022-11-29 12:45:00 +0530

    [Mohana] Day4 assignment SnakeAndLadder

---
+++ src/UC2RandomValue.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+public class UC2RandomValue {
+        public static void main(String[] args) {
+
+            Random rand = new Random();
+            int diceValue = rand.nextInt(6)+1;
+            System.out.println("Dice Value is " +diceValue);
+        }
+}
